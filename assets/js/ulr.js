3a7dbc2372ec3634824628535be56df704cbff20
Simona Dumascu <76841254+SimonaDumascu@users.noreply.github.com>
2021-12-25 13:03:57 +0100

    Mobile

---
+++ assets/js/ulr.js
@@ -0,0 +1,15 @@
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+
+if (urlParams.has('graphic_design')) {
+    console.log('graphic_design');
+    document.getElementById('graphic_design').checked = true;
+}
+if (urlParams.has('ux_ui')) {
+    console.log('ux_ui');
+    document.getElementById('ux_ui').checked = true;
+}
+if (urlParams.has('concept_art')) {
+    console.log('concept_art');
+    document.getElementById('concept_art').checked = true;
+}
